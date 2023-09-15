5bc3c5240b6001c3ec49a6ae71f468fe379d7376
Matheus Gabriel <math.gsilva07@gmail.com>
2023-09-15 14:28:54 -0300

    Update App.js

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -7,14 +7,15 @@ import "./App.css";
 const API_URL = "https://omdbapi.com?apikey=7b62cb24";
 const App = () => {
   const [movies, setMovies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
     setMovies(data.Search);
   };
   useEffect(() => {
-    searchMovies("All");
+    searchMovies(searchTerm);
   }, []);
 
   return (
