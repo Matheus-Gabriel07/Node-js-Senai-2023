a206d11f470d2975ca8206083a315727114b5190
Matheus Gabriel <math.gsilva07@gmail.com>
2023-09-15 09:09:27 -0300

    Summary

---
+++ movie-app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  URL("http://www.omdbapi.com/?i=tt3896198&apikey=7b62cb24")
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
